29c97843a5fce5a7690a771e7ab7ad86059e082e
Thomas Marlow <thomas_marlow@brown.edu>
2016-09-20 15:05:49 -0400

    Defined basic tri function

--- code/test_functions.R
+++ code/test_functions.R
@@ -1,5 +1,6 @@
 library(httr)
 library(readr)
+library(stringr)
 
 url <-  "https://iaspub.epa.gov/"
 #path <- "enviro/efservice/tri_facility/state_abbr/RI/CSV"
@@ -14,3 +15,19 @@ ri <- read.csv(text = content, stringsAsFactors = FALSE)
 
 #setwd("~/Google\ Drive/epapi/")
 #write_csv(ri, "temp.csv")
+
+
+
+tri <- function(state_abbreviation = "", year = ""){
+    url <-  "https://iaspub.epa.gov/"
+    api <- "enviro/efservice/tri_facility_history/"
+    state <- str_c("state_abbr/", state_abbreviation)
+    year <- str_c("/REPORTING_YEAR/", year)
+    path <- str_c(api, state, year, "/CSV")
+    raw <- GET(url = url, path = path)
+    content <- rawToChar(raw$content)
+    df <- read.csv(text = content, stringsAsFactors = FALSE)
+    return(df)
+}
+
+ri90 <- tri("RI", "1990")
