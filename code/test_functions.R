adb8c0f8040f62b492f84fbaa552c5579370d8bc
Thomas Marlow <thomas_marlow@brown.edu>
2016-09-20 13:32:48 -0400

    Now able to specify reporting year.

--- code/test_functions.R
+++ code/test_functions.R
@@ -1,11 +1,16 @@
 library(httr)
-
+library(readr)
 
 url <-  "https://iaspub.epa.gov/"
-path <- "enviro/efservice/tri_form_r/rptyear/ 2007/tri_facility/state_abbr/RI/CSV"
+#path <- "enviro/efservice/tri_facility/state_abbr/RI/CSV"
+path <-  "enviro/efservice/tri_facility_history/state_abbr/RI/CSV"
+
 
 raw.result <- GET(url = url, path = path)
 
 content <- rawToChar(raw.result$content)
 
-ri <- read.csv(text = this.raw.content)
+ri <- read.csv(text = content, stringsAsFactors = FALSE)
+
+#setwd("~/Google\ Drive/epapi/")
+#write_csv(ri, "temp.csv")
