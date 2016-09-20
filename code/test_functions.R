c5a70c8525f3bd37347cd01a223d4e27981cd4c4
Thomas Marlow <thomas_marlow@brown.edu>
2016-09-20 11:47:51 -0400

    basic api functions are now working.

--- code/test_functions.R
+++ code/test_functions.R
@@ -2,7 +2,7 @@ library(httr)
 
 
 url <-  "https://iaspub.epa.gov/"
-path <- "enviro/efservice/tri_facility/state_abbr/RI/CSV"
+path <- "enviro/efservice/tri_form_r/rptyear/ 2007/tri_facility/state_abbr/RI/CSV"
 
 raw.result <- GET(url = url, path = path)
 
