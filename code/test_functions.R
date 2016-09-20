4c4919c527598408103d40551296deed284248ae
Thomas Marlow <thomas_marlow@brown.edu>
2016-09-20 16:27:19 -0400

    TRI reporting form function. buggy release ftn

--- code/test_functions.R
+++ code/test_functions.R
@@ -4,17 +4,18 @@ library(stringr)
 
 url <-  "https://iaspub.epa.gov/"
 #path <- "enviro/efservice/tri_facility/state_abbr/RI/CSV"
-path <-  "enviro/efservice/tri_facility_history/state_abbr/RI/CSV"
+#path <-  "enviro/efservice/tri_facility_history/state_abbr/RI/CSV"
 
+path <- "enviro/efservice/tri_reporting_form/TRI_FACILITY_ID/02808BRDFR1712B/CSV"
+
+path <- "enviro/efservice/tri_release_qty/DOC_CTRL_NUM/1393075454621/CSV"
 
 raw.result <- GET(url = url, path = path)
-
 content <- rawToChar(raw.result$content)
-
 ri <- read.csv(text = content, stringsAsFactors = FALSE)
 
-#setwd("~/Google\ Drive/epapi/")
-#write_csv(ri, "temp.csv")
+setwd("~/Google\ Drive/epapi/")
+write_csv(df3, "temp.csv")
 
 
 
@@ -31,3 +32,37 @@ tri <- function(state_abbreviation = "", year = ""){
 }
 
 ri90 <- tri("RI", "1990")
+
+## Get a list of facility ids from the resulting df
+
+ids <- unique(ri90$tri_facility_history.TRI_FACILITY_ID)
+
+## foreach facility id, query tri_reporting form
+## first need to define a new function for this to use in lapply
+tri_reporting_form <- function(id){
+    url <-  "https://iaspub.epa.gov/"
+    api <- "enviro/efservice/tri_reporting_form/TRI_FACILITY_ID/"
+    path <- str_c(api, id, "/CSV")
+    raw <- GET(url = url, path = path)
+    content <- rawToChar(raw$content)
+    df <- read.csv(text = content, stringsAsFactors = FALSE)
+    return(df)
+}
+
+df2 <- lapply(ids, tri_reporting_form)
+df3 <- do.call(rbind, df2)
+
+
+#doc_nums <- unique(df3$tri_reporting_form.DOC_CTRL_NUM)
+#tri_release_qty <- function(doc_ids){
+##     url <-  "https://iaspub.epa.gov/"
+##     api <- "enviro/efservice/tri_release_qty/DOC_CTRL_NUM/"
+##     path <- str_c(api, doc_ids, "/CSV")
+##     raw <- GET(url = url, path = path)
+##     content <- rawToChar(raw$content)
+##     df <- read.csv(text = content, stringsAsFactors = FALSE)
+##     return(df)
+## }
+
+#df4 <- lapply(doc_nums, tri_release_qty) # this didn't work! Failed!
+#df5 <- do.call(rbind, df4)
